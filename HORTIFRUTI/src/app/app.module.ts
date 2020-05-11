48331371a971b05954a1e251d460eed7ffdcc744
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-05-11 10:32:00 -0300

    Criado os componentes home, loja, navbar, footer

--- HORTIFRUTI/src/app/app.module.ts
+++ HORTIFRUTI/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LojaComponent } from './loja/loja.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LojaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
