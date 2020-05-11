55623478365362d9bb7b2cf153ea8b08496c2144
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-05-11 12:48:56 -0300

    Criação do EndPoint AllProdutos

--- HORTIFRUTI/src/app/service/produto.service.ts
+++ HORTIFRUTI/src/app/service/produto.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllProdutos(){
+    return this.http.get('http://31.220.57.121:9080/produtos/')
+  }
+
+
+
 }
