9bb1bcbed800372faab4344aa4ad8c0169e74cdd
Ivan Júnior Carlota <ivan.junior.carlota@gmail.com>
2020-05-11 15:31:31 -0300

    Criação do componente put-produto e implementação do EndPoint putProduto

--- HORTIFRUTI/src/app/app-routing.module.ts
+++ HORTIFRUTI/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LojaComponent } from './loja/loja.component';
+import { PutProdutoComponent } from './put-produto/put-produto.component';
 
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path:'loja', component: LojaComponent}
+  {path:'loja', component: LojaComponent},
+  {path: 'put-produto/:id', component: PutProdutoComponent}
 ];
 
 @NgModule({
--- HORTIFRUTI/src/app/service/produto.service.ts
+++ HORTIFRUTI/src/app/service/produto.service.ts
@@ -18,4 +18,11 @@ export class ProdutoService {
     return this.http.post('http://31.220.57.121:9080/produtos/', produto)
   }
 
+  putProduto(produto: Produto){
+    return this.http.put('http://31.220.57.121:9080/produtos/', produto)
+  }
+
+  getByIdProduto(id:number){
+    return this.http.get(`http://31.220.57.121:9080/produtos/${id}`)
+  }
 }
